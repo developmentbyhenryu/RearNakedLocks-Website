f80e817b6734900453f82a738724286d1cbd9fd6
developmentbyhenryu <developmentbyhenryu@gmail.com>
2026-07-06 01:04:17 -0700

    event update

--- rear-naked-locks/data/events.ts
+++ rear-naked-locks/data/events.ts
@@ -4,6 +4,526 @@ export const events = [
        BASIC EVENT INFO
     ========================= */
 
+    slug: "ufc-329",
+
+    name: "UFC 329",
+
+    shortName: "UFC 329",
+
+    date: "July 11, 2026",
+
+    location: "Las Vegas, Nevada United States",
+
+    venue: "T-Mobile Arena",
+
+    status: "upcoming",
+    // upcoming | live | completed
+
+    poster: "/events/poster.jpg",
+
+    banner: "/events/poster.jpg",
+
+    countdown: true,
+
+    /* =========================
+       MAIN EVENT
+    ========================= */
+
+    mainEvent: {
+      fighterA: {
+        name: "Conor McGregor",
+
+        image:
+          "/fighters/conor-mcgregor.png",
+
+        record: "22-6",
+
+        country: "Ireland",
+      },
+
+      fighterB: {
+        name: "Max Holloway",
+
+        image:
+          "/fighters/max-holloway.png",
+
+        record: "27-9",
+
+        country: "USA",
+      },
+
+      weightClass: "Welterweight",
+
+      rounds: 5,
+    },
+
+    /* =========================
+       CO-MAIN EVENT
+    ========================= */
+
+    coMainEvent: {
+      fighterA: {
+        name: "Benoit Saint Denis",
+
+        image:
+          "/fighters/benoit-saint-denis.png",
+      },
+
+      fighterB: {
+        name: "Paddy Pimblett",
+
+        image:
+          "/fighters/paddy-pimblett.png",
+      },
+
+      weightClass: "Lightweight",
+
+      rounds: 3,
+    },
+
+    /* =========================
+       BREAKDOWN VIDEO
+    ========================= */
+
+    breakdownVideo: {
+      youtubeId: "5B3-wKlUCCk",
+
+      title:
+        "UFC Freedom 250 Full Betting Breakdown",
+    },
+ /* =========================
+       Individual PICKS
+    ========================= */
+
+    individualPicks: {
+  henry: [
+    {
+      fighter: "Max Holloway",
+      odds: "-245",
+      confidence: 8,
+    },
+
+    {
+      fighter: "Paddy Pimblett",
+      odds: "+126",
+      confidence: 8,
+    },
+
+    {
+      fighter: "Mario Bautista",
+      odds: "+126",
+      confidence: 7,
+    },
+    {
+      fighter: "Loneer Kavanagh",
+      odds: "-225",
+      confidence: 9,
+    },
+    {
+      fighter: "King Green",
+      odds: "+126",
+      confidence: 5,
+    },
+    {
+      fighter: "Gable Steveson",
+      odds: "-2500",
+      confidence: 10,
+    },
+    {
+      fighter: "Robert Whittaker",
+      odds: "-174",
+      confidence: 8,
+    },
+    {
+      fighter: "Adrian Yanez",
+      odds: "-440",
+      confidence: 7,
+    },
+    {
+      fighter: "Luke Riley",
+      odds: "-340",
+      confidence: 8,
+    },
+    {
+      fighter: "Tracy Cortez",
+      odds: "-104",
+      confidence: 6,
+    },
+    {
+      fighter: "Damian Pinas",
+      odds: "-215",
+      confidence: 6,
+    },
+    {
+      fighter: "Ryan Gandra",
+      odds: "-150",
+      confidence: 8,
+    },
+    {
+      fighter: "Alessandro Costa",
+      odds: "-265",
+      confidence: 8,
+    },
+  ],
+
+  chato: [
+     {
+      fighter: "Max Holloway",
+      odds: "-245",
+      confidence: 8,
+    },
+
+    {
+      fighter: "Paddy Pimblett",
+      odds: "+126",
+      confidence: 8,
+    },
+
+    {
+      fighter: "Mario Bautista",
+      odds: "+126",
+      confidence: 7,
+    },
+    {
+      fighter: "Loneer Kavanagh",
+      odds: "-225",
+      confidence: 9,
+    },
+    {
+      fighter: "Terrance Mckinney",
+      odds: "-162",
+      confidence: 5,
+    },
+    {
+      fighter: "Gable Steveson",
+      odds: "-2500",
+      confidence: 10,
+    },
+    {
+      fighter: "Robert Whittaker",
+      odds: "-174",
+      confidence: 8,
+    },
+    {
+      fighter: "Adrian Yanez",
+      odds: "-440",
+      confidence: 7,
+    },
+    {
+      fighter: "Luke Riley",
+      odds: "-340",
+      confidence: 8,
+    },
+    {
+      fighter: "Tracy Cortez",
+      odds: "-104",
+      confidence: 6,
+    },
+    {
+      fighter: "Damian Pinas",
+      odds: "-215",
+      confidence: 6,
+    },
+    {
+      fighter: "Ryan Gandra",
+      odds: "-150",
+      confidence: 8,
+    },
+    {
+      fighter: "Alessandro Costa",
+      odds: "-265",
+      confidence: 8,
+    },
+  ],
+},
+   
+
+    officialPicks: {
+      playOfTheWeek: {
+        henryPick:
+          "Loneer Kavanagh",
+
+        chatoPick:
+          "Alessandro Costa",
+
+        odds: "-101",
+
+        result: null,
+        // win | loss | push
+
+        units: 0,
+      },
+
+      barkAlley: {
+        fighter:
+          "Paddy Pimblett",
+
+        odds: "+126",
+
+        result: null,
+
+        methodOfVictory: null,
+
+        methodOfLoss: null,
+
+        units: 0,
+      },
+
+      aroundTheWorld: {
+        countries: [
+          {
+            country: "England",
+
+            fighter:
+              "Luke Riley",
+
+            result: null,
+          },
+
+          {
+            country: "USA",
+
+            fighter:
+              "Max Holloway",
+
+            result: null,
+          },
+
+          {
+            country: "Brazil",
+
+            fighter:
+              "Alessandro Costa",
+
+            result: null,
+          },
+        ],
+
+        parlayOdds: "+151",
+
+        result: null,
+
+        units: 0,
+      },
+
+      allAction: {
+        legs: [
+  {
+    fighter: "Robert Whittaker",
+    method: "",
+    result: null,
+  },
+  {
+    fighter: "Ryan Gandra",
+    method: "",
+    result: null,
+  },
+],
+
+        parlayOdds: "+157",
+
+        result: null,
+
+        units: 0,
+      },
+    },
+
+    /* =========================
+       FULL FIGHT CARD
+    ========================= */
+
+    fightCard: {
+      mainCard: [
+        {
+          fighterA:
+            "Ilia Topuria",
+
+          fighterB:
+            "Justin Gaethje",
+
+          weightClass:
+            "Lightweight",
+
+          predictedWinner:
+            "Ilia Topuria",
+
+          confidence: 8,
+
+          result: null,
+        },
+
+        {
+          fighterA:
+            "Alex Pereira",
+
+          fighterB:
+            "Cirly Gane",
+
+          weightClass:
+            "Heavyweight",
+
+          predictedWinner:
+            "Ciryl Gane",
+
+          confidence: 9,
+
+          result: null,
+        },
+        {
+          fighterA:
+            "Sean O'Malley",
+
+          fighterB:
+            "Aiemann Zahabi",
+
+          weightClass:
+            "Bantamweight",
+
+          predictedWinner:
+            "Sean O'Malley",
+
+          confidence: 9,
+
+          result: null,
+        },
+        {
+          fighterA:
+            "Josh Hokit",
+
+          fighterB:
+            "Derick Lewis",
+
+          weightClass:
+            "Heavyweight",
+
+          predictedWinner:
+            "Josh Hokit",
+
+          confidence: 9,
+
+          result: null,
+        },
+        {
+          fighterA:
+            "Mauricio Ruffy",
+
+          fighterB:
+            "Michael Chandler",
+
+          weightClass:
+            "Lightweight",
+
+          predictedWinner:
+            "Mauricio Ruffy",
+
+          confidence: 9,
+
+          result: null,
+        },
+        {
+          fighterA:
+            "Bo Nickal",
+
+          fighterB:
+            "Kyle Daukaus",
+
+          weightClass:
+            "Middleweight",
+
+          predictedWinner:
+            "Bo Nickal",
+
+          confidence: 9,
+
+          result: null,
+        },
+        {
+          fighterA:
+            "Diego Lopes",
+
+          fighterB:
+            "Steve Garcia",
+
+          weightClass:
+            "Featherweight",
+
+          predictedWinner:
+            "Diego Lopes",
+
+          confidence: 9,
+
+          result: null,
+        },
+      ],
+
+      prelims: [
+        {
+          fighterA:
+            "",
+
+          fighterB:
+            "",
+
+          weightClass:
+            "",
+
+          predictedWinner:
+            " ",
+
+          confidence: 0,
+
+          result: null,
+        },
+      ],
+    },
+
+    /* =========================
+       EVENT RESULTS
+    ========================= */
+
+    results: {
+      totalUnits: 0,
+
+      wins: 0,
+
+      losses: 0,
+
+      pushes: 0,
+
+      roi: 0,
+    },
+
+    /* =========================
+       ARTICLES
+    ========================= */
+
+    articles: [
+      {
+        title:
+          "Why Islam Makhachev dominates this matchup",
+
+        slug:
+          "islam-makhachev-breakdown",
+      },
+    ],
+
+    /* =========================
+       SEO
+    ========================= */
+
+    seo: {
+      title:
+        "UFC 329 Betting Picks, Predictions & Breakdown | Rear Naked Locks",
+
+      description:
+        "Full UFC 329 betting breakdown including official picks, parlays, underdogs, analytics, and predictions from Rear Naked Locks.",
+    },
+  },
+  {
+    /* =========================
+       BASIC EVENT INFO
+    ========================= */
+
     slug: "ufc-freedom-250",
 
     name: "UFC Freedom 250",
